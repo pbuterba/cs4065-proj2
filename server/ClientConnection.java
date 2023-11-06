a225640a536ed49d0e698e5441963c2d89a34576
Preston Buterbaugh <pbuterba.corp@gmail.com>
2023-11-06 16:53:25 -0500

    #18 - Add basic functions for command processing on the server

--- server/ClientConnection.java
+++ server/ClientConnection.java
@@ -1,5 +1,7 @@
 package server;
 
+import java.io.*;
+import java.util.*;
 import java.net.*;
 public class ClientConnection implements Runnable {
     //Fields
@@ -12,7 +14,35 @@ public class ClientConnection implements Runnable {
 
     //Methods
     public void run() {
-        // TODO add code to handle various messages that may be received from a client  
+        try {
+            waitForClientData();
+        } except(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private void waitForClientData() throws Exception {
+        //Get line of text sent from client
+        BufferedReader streamFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String dataLine = streamFromClient.readLine();
+
+        //Loop until the text "exit" is sent
+        while(!dataLine.equals("exit")) {
+            //Split the input line into the command and any following arguments
+            StringTokenizer tokens = new StringTokenizer(dataLine);
+            String command = tokens.nextToken();
+            ArrayList<String> args = new ArrayList<String>(tokens.countTokens() - 1);
+            while(tokens.hasMoreTokens()) {
+                args.add(tokens.nextToken());
+            }
+
+            //Take action based on what command was entered
+            
+        }
+        
+        //Close the socket
+        System.out.println(socket.getRemoteSocketAddress() + " disconnected");
+        socket.close();
     }
 
     public static void addUser() {}
