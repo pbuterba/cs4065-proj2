ecca25668dde688c687e9b8d92f9d94dd2ad72ef
Preston Buterbaugh <pbuterba.corp@gmail.com>
2023-11-06 21:07:28 -0500

    #18, #6 - Re-structure server to have main function and run thread function Implement "join" command functionality on server

--- server/Server.java
+++ server/Server.java
@@ -12,16 +12,24 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 import server.Message;
-import server.ClientConnection;
-public class Server {
+public class Server implements Runnable {
+    //Static server data structures
+    public static ArrayList<Socket> connectedClients = new ArrayList<Socket>();
+    public static ArrayList<String> connectedUsers = new ArrayList<String>();
+    public static HashMap<String, Message> messages = new HashMap<String, Message>();
+    
+    //Field for the client connection socket (used in threads)
+    private Socket socket;
+
+    //Constructor for use when starting new threads
+    public Server(Socket clientSocket) {
+        this.socket = clientSocket;
+    }
+
+    //Main function - Listens for connection requests and then 
     public static void main(String[] args) throws Exception {
         //Create server socket
         ServerSocket serverSocket = new ServerSocket(6789);
-
-        //Create data structure to track connected clients, connected users, and messages sent
-        ArrayList<ClientConnection> connectedClients = new ArrayList<ClientConnection>();
-        ArrayList<String> connectedUsers = new ArrayList<String>();
-        HashMap<String, Message> messages = new HashMap<String, Message>();
 
         //Print message to show that server program has successfully started
         System.out.println("Server started");
@@ -29,11 +37,58 @@ public class Server {
         //Loop infinetely until server is closed
         while(true) {
             Socket clientSocket = serverSocket.accept();
-            ClientConnection newClient = new ClientConnection(clientSocket);
+            Server clientConnection = new Server(clientSocket);
             System.out.println("New client connected with address " + clientSocket.getRemoteSocketAddress());
-            connectedClients.add(newClient);
-            Thread thread = new Thread(newClient);
+            connectedClients.add(clientSocket);
+            Thread thread = new Thread(clientConnection);
             thread.start();
         }
     }
+
+    //Run function - runs threads
+    public void run() {
+        try {
+            waitForClientData();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private void waitForClientData() throws Exception {
+        //Get line of text sent from client
+        BufferedReader streamFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String dataLine = streamFromClient.readLine();
+
+        //Loop until the text "exit" is sent
+        while(!dataLine.equals("exit")) {
+            //Split the input line into the command and any following arguments
+            StringTokenizer tokens = new StringTokenizer(dataLine);
+            String command = tokens.nextToken();
+            ArrayList<String> args = new ArrayList<String>(tokens.countTokens() - 1);
+            while(tokens.hasMoreTokens()) {
+                args.add(tokens.nextToken());
+            }
+
+            //Take action based on what command was entered
+            if(command.equals("join")) {
+                addUser(args.get(0));
+            }
+        }
+        
+        //Close the socket
+        System.out.println(socket.getRemoteSocketAddress() + " disconnected");
+        socket.close();
+    }
+
+    //Function for user joining the group
+    public static void addUser(String username) {
+        System.out.println(username + " issued join command");
+        connectedUsers.add(username);
+    }
+
+    public static void createPost() {}
+    public static void sendUserList() {}
+    public static void removeUser() {}
+    public static void retrieveMessage() {}
+    public static void closeServer() {}
 }
